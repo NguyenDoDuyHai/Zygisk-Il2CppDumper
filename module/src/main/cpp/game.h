01939771f1166291a23f06bff6e8f7d92eab439e
DuyHai <96824625+NguyenDoDuyHai@users.noreply.github.com>
2025-03-04 17:06:54 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,20 +5,8 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#include <fstream>
-#include <string>
+#define GamePackageName "com.game.packagename"
 
-std::string getPackageNameFromFile() {
-    std::ifstream file("/data/local/tmp/game_package.txt");
-    std::string packageName;
-    if (file.is_open()) {
-        std::getline(file, packageName);  // đọc đúng dòng đầu tiên
-        file.close();
-    } else {
-        packageName = "com.default.game";  // fallback khi file không tồn tại hoặc lỗi
-    }
-    return packageName;
-}
 
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
